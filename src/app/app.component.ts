2001bd2867f4d22986f7713a426a1dae713559ac
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-04-20 19:27:08 +0200

    cities list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather-app';
+  cities = [];
+  matchedCities = [];
+  searchString = '';
+
+  constructor(private http: HttpClient) {
+
+    this.http.get('./assets/world-cities.json')
+      .subscribe(cities => {
+        this.cities = cities;
+        this.showCities();
+      });
+
+  }
+
+  onKey(event) {
+    this.searchString = event.target.value;
+    this.showCities();
+  }
+
+  showCities() {
+    const matched = this.cities
+      .map(city => ({city, lc: city.toLowerCase()}))
+      .filter(({lc}) => lc.includes(this.searchString))
+      .slice(0, 100)
+      .map(({city}) => city);
+
+    this.matchedCities = matched;
+  }
+
 }
+
