ade3b67e285b2a8e76d943429823d308e2f5733a
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-04-20 23:46:46 +0200

    add material elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { StorageModule } from '@ngx-pwa/local-storage';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,6 +26,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HttpClientModule,
     FontAwesomeModule,
     StorageModule.forRoot({ IDBNoWrap: true }),
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
