4f0d7b6145d356b88ea3fe286330678883d7f2e9
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-04-22 18:25:45 +0200

    add remove tile button

--- src/app/dashboard/tile/tile.component.ts
+++ src/app/dashboard/tile/tile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { Router } from '@angular/router';
 import { UtilsService } from '../../services/utils.service';
+import { faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { FavouriteService } from '../../services/favourite.service';
 
 @Component({
   selector: 'app-tile',
@@ -12,11 +14,13 @@ export class TileComponent implements OnInit, OnChanges {
   @Input()
   city: string;
   data = {};
+  faTrashAlt = faTrashAlt;
 
   constructor(
     private weather: WeatherService,
     private router: Router,
-    private utils: UtilsService) { }
+    private utils: UtilsService,
+    private favourite: FavouriteService) { }
 
   ngOnChanges() {
     this.weather.getShort(this.city).subscribe(data => this.data = data);
@@ -24,6 +28,11 @@ export class TileComponent implements OnInit, OnChanges {
 
   redirect(city) {
     this.utils.urlNormalize(city).then(url => this.router.navigate(['/city', url]));
+  }
+
+  removeFav(city, event) {
+    this.favourite.toggleCity(city);
+    event.stopPropagation();
   }
 
   ngOnInit() {
